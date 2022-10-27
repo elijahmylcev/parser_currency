539d4ea07a8d5b5f071bc09e2fcaee345238112f
Elijah Mylcev <elijahmylcev@gmail.com>
2022-10-27 21:48:15 +0600

    clear parser

--- parser-page.py
+++ parser-page.py
@@ -6,35 +6,29 @@ from config import page, user_agent
 from selenium import webdriver
 
 
-# headers = {'User-agent': user_agent}
-# full_page = requests.get(page, headers=headers)
-# soup = BeautifulSoup(full_page.content, 'lxml')
-#
-# print(soup)
+def get_data_with_selenium(page_url):
+  options = webdriver.ChromeOptions()
 
-# def get_data_with_selenium(page_url):
-#   options = webdriver.ChromeOptions()
-#
-#   try:
-#     driver = webdriver.Chrome(
-#       executable_path='chromedriver.exe',
-#       options=options
-#     )
-#     driver.get(url=page_url)
-#     sleep(10)
-#
-#     with open('index.html', 'w', encoding="utf-8") as file:
-#       file.write(driver.page_source)
-#
-#   except Exception as ex:
-#     print(ex)
-#   finally:
-#     driver.close()
-#     driver.quit()
-#
-#
-# # def main():
-# get_data_with_selenium(page_url=page)
+  try:
+    driver = webdriver.Chrome(
+      executable_path='chromedriver.exe',
+      options=options
+    )
+    driver.get(url=page_url)
+    sleep(10)
+
+    with open('index.html', 'w', encoding="utf-8") as file:
+      file.write(driver.page_source)
+
+  except Exception as ex:
+    print(ex)
+  finally:
+    driver.close()
+    driver.quit()
+
+
+# def main():
+get_data_with_selenium(page_url=page)
 
 with open('index.html', 'r', encoding="utf-8") as file:
   src = file.read()
@@ -53,7 +47,6 @@ print(result_values)
 maximum = max(result_values)
 
 print(maximum)
-
 
 
 # if __name__ == '__main__':
