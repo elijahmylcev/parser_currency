445c154c3592ea17c7ebd2a12e2801f1ac33057d
Elijah Mylcev <elijahmylcev@gmail.com>
2022-10-27 21:22:28 +0600

    currency course

--- parser-page.py
+++ parser-page.py
@@ -12,29 +12,40 @@ from selenium import webdriver
 #
 # print(soup)
 
-def get_data_with_selenium(page_url):
-  options = webdriver.ChromeOptions()
+# def get_data_with_selenium(page_url):
+#   options = webdriver.ChromeOptions()
+#
+#   try:
+#     driver = webdriver.Chrome(
+#       executable_path='chromedriver.exe',
+#       options=options
+#     )
+#     driver.get(url=page_url)
+#     sleep(10)
+#
+#     with open('index.html', 'w', encoding="utf-8") as file:
+#       file.write(driver.page_source)
+#
+#   except Exception as ex:
+#     print(ex)
+#   finally:
+#     driver.close()
+#     driver.quit()
+#
+#
+# # def main():
+# get_data_with_selenium(page_url=page)
 
-  try:
-    driver = webdriver.Chrome(
-      executable_path='chromedriver.exe',
-      options=options
-    )
-    driver.get(url=page_url)
-    sleep(10)
+with open('index.html', 'r', encoding="utf-8") as file:
+  src = file.read()
 
-    with open('index.html', 'w', encoding="utf-8") as file:
-      file.write(driver.page_source)
+soup = BeautifulSoup(src, 'lxml')
 
-  except Exception as ex:
-    print(ex)
-  finally:
-    driver.close()
-    driver.quit()
+el = soup.find_all('span', {'title': 'RUB - покупка'})
+
+print(el)
 
 
-# def main():
-get_data_with_selenium(page_url=page)
 
 # if __name__ == '__main__':
 #   main()
